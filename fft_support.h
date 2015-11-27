a1fe2a3d3f7ec080d2f365a39654c37ec6e18566
Andrew Janke <a.janke@gmail.com>
2015-11-28 07:30:14 +1000

    builds with cmake

--- fft_support.h
+++ fft_support.h
@@ -1,29 +1,12 @@
-/* ----------------------------- MNI Header -----------------------------------
-@NAME       : fft_support.h
-@DESCRIPTION: defines and prototypes for fft support routines.
-@CREATED    : Fri Nov  5 11:16:54 EST 1993 Louis Collins
-@MODIFIED   : $Log: fft_support.h,v $
-@MODIFIED   : Revision 1.1  2002-09-17 23:44:51  rotor
-@MODIFIED   : ----------------------------------------------------------------------
-@MODIFIED   : Initial entry of mincfft to CVS (to perhaps replace louis's old NR
-@MODIFIED   :    version of mincfft
-@MODIFIED   :
-@MODIFIED   : Committing in .
-@MODIFIED   :
-@MODIFIED   : Added Files:
-@MODIFIED   :  Makefile fft_support.c fft_support.h mincfft.c
-@MODIFIED   : ----------------------------------------------------------------------
-@MODIFIED   :
- * Revision 1.1  93/11/05  14:21:07  louis
- * Initial revision
- *
----------------------------------------------------------------------------- */
+/* fft_support.h */
 
 #ifndef FFT_SUPPORT_H
 #define FFT_SUPPORT_H
 
 
 #include <minc2.h>
+#include <volume_io.h>
+
 
 #define   MAX_OUTFILES           8
 #define   OUTPUT_REAL_AND_IMAG   0
@@ -36,9 +19,9 @@
 #define   OUTPUT_POWER           7
 
 
-Status prep_volume(Volume *tmp, Volume *data);
-Status proj_volume(Volume *tmp, Volume *data, int job);
-Status fft_volume(Volume data, int inverse_flg, int dim, int centre);
+VIO_Status prep_volume(VIO_Volume *tmp, VIO_Volume *data);
+VIO_Status proj_volume(VIO_Volume *tmp, VIO_Volume *data, int job);
+VIO_Status fft_volume(VIO_Volume data, int inverse_flg, int dim, int centre);
 
 
 
